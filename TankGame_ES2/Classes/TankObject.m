a1a9e9fb0f3981ec53902a29ad17fb5799237a13
BjornC <bjorn@BjornCs-MacBook-Pro.local>
2013-07-04 16:41:58 -0700

    More tank object

--- TankGame_ES2/Classes/TankObject.m
+++ TankGame_ES2/Classes/TankObject.m
@@ -16,23 +16,33 @@
     self = [super init];
     if (self) {
         // physics engine
-        cpFloat radius = 1;
-        cpFloat mass = 1;
-        cpFloat moment = cpMomentForCircle(mass, 0, radius, cpvzero);
+        //cpFloat radius = 1;
+        cpFloat width = 2;
+        cpFloat height = 2;
+        cpFloat mass = 5;
+        //cpFloat moment = cpMomentForCircle(mass, 0, radius, cpvzero);
+        cpFloat moment = cpMomentForBox(mass, width, height);
         self.space = obj_space;
         self.body = cpSpaceAddBody(obj_space, cpBodyNew(mass, moment));
         cpBodySetPos(self.body, pos);
         cpBodySetVel(self.body, vel);
-        self.shape = cpSpaceAddShape(obj_space, cpCircleShapeNew(self.body, radius, cpvzero));
+        self.shape = cpSpaceAddShape(obj_space, cpBoxShapeNew(self.body, width, height));
+        //self.shape = cpSpaceAddShape(obj_space, cpCircleShapeNew(self.body, radius, cpvzero));
         cpShapeSetFriction(self.shape, 0.7);
         cpShapeSetElasticity(self.shape, 0.7);
         
         // GL rendering
         self.shaderProgHandle = shade;
-        self.coords = (void*)&tankShell[0].Position[0];
-        self.colors = (void*)&tankShell[0].Color[0];
+        // turret
+        self.turretCoords = (void*)&turretVerts[0].Position[0];
+        self.turretColors = (void*)&turretVerts[0].Color[0];
+        self.turretStride = sizeof(Vertex);
+        self.turretVertCount = sizeof(turretVerts)/sizeof(Vertex);
+        // tank body
+        self.tankCoords = (void*)&tankVerts[0].Position[0];
+        self.tankColors = (void*)&tankVerts[0].Color[0];
         self.tankStride = sizeof(Vertex);
-        self.tankVertCount = sizeof(tankShell)/sizeof(Vertex);
+        self.tankVertCount = sizeof(tankVerts)/sizeof(Vertex);
     }
     return self;
     
@@ -54,8 +64,8 @@
     glEnableVertexAttribArray(positionSlot);
     glEnableVertexAttribArray(colorSlot);
     
-    glVertexAttribPointer(positionSlot, 2, GL_FLOAT, GL_FALSE, self.tankStride, self.coords);
-    glVertexAttribPointer(colorSlot, 4, GL_FLOAT, GL_FALSE, self.tankStride, self.colors);
+    //glVertexAttribPointer(positionSlot, 2, GL_FLOAT, GL_FALSE, self.tankStride, self.coords);
+    //glVertexAttribPointer(colorSlot, 4, GL_FLOAT, GL_FALSE, self.tankStride, self.colors);
     glDrawArrays(GL_LINE_LOOP, 0, self.tankVertCount);
     glDisableVertexAttribArray(positionSlot);
     glDisableVertexAttribArray(colorSlot);
